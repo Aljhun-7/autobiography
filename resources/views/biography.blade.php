fcd7635e1711bc321e134632f1cf6f6f76a8ad13
Aljhun-7 <aljhunangala7@gmail.com>
2025-09-02 15:18:21 +0800

    Create biography.blade.php

---
+++ resources/views/biography.blade.php
@@ -0,0 +1,56 @@
+<x-layout>
+    <main>
+        <div class="album py-5 mt-5 bg-custom">
+            <div class="container">
+
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+                    <div class="col px-4">
+                        <a class="card shadow-sm" href="#">
+                            <img class="bd-placeholder-img card-img-top rounded-circle" width="100%" height="225" src="#" alt="angala"role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <div class="card-body">
+                                <p class="card-text">Angala, Aljhun</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <small class="text-body-secondary">aljhunangala7@gmail.com</small>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col px-4">
+                        <a class="card shadow-sm" href="#">
+                            <img class="bd-placeholder-img card-img-top rounded-circle" width="100%" height="225" src="#" alt="hipolito"role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <div class="card-body">
+                                <p class="card-text">Hipolito, Charlene</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <small class="text-body-secondary">charlenehipolito19@gmail.com</small>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col px-4">
+                        <a class="card shadow-sm" href="#">
+                            <img class="bd-placeholder-img card-img-top rounded-circle" width="100%" height="225" src="#" alt="marzan"role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <div class="card-body">
+                                <p class="card-text">Marzan, Mark Jade</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <small class="text-body-secondary">markjademarzan@gmail.com</small>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col px-4">
+                        <a class="card shadow-sm" href="#">
+                            <img class="bd-placeholder-img card-img-top rounded-circle" width="100%" height="225" src="#" alt="wong"role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <div class="card-body">
+                                <p class="card-text">Wong, Alyza Kaye</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <small class="text-body-secondary">wongallyzakaye@gmail.com</small>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+</x-layout>
