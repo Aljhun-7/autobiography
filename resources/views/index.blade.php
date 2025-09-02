1940e31aaa9bfbab7e247e7e8657c7b4a944aee7
Aljhun-7 <aljhunangala7@gmail.com>
2025-09-02 15:12:55 +0800

    Create index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <main>
+        <div class="container bg-custom-light p-5 my-5">
+            <div class="header-style mb-5">
+                <h1 class="m-0 poppins-semibold">Think Twice,<br> Code Once</h1><br>
+                <h5 class="m-0 poppins-medium"> BSIT 3A <br> Angala, Aljhun <br> Marzan, Mark Jade <br> Hipolito, Charlene <br> Wong, Alyza Kaye </h5>
+            </div>
+        </div>
+
+    </main>
+</x-layout>
