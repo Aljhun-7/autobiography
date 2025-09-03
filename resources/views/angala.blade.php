feaf233a87a45db65041353dd8d7f1f2e126c8e4
Aljhun-7 <aljhunangala7@gmail.com>
2025-09-03 15:49:56 +0800

    Create angala.blade.php

---
+++ resources/views/angala.blade.php
@@ -0,0 +1,35 @@
+<x-navbtn>
+<main><body class=" bg-success-subtle">
+	<div class="p-md-2 m-md-3">
+		<div class="card mb-md-3 rounded-pill p-4 shadow-lg border-3 border-success" style="max-width: 540px;">
+			<div class="row g-0">
+			    <div class="col-md-4">
+			    	<img src="{{ asset('asset/angala_ab.jpg') }}" class="img-fluid rounded-circle border-2 border-success border-opacity-75" alt="aljhun pic">
+			    </div>
+			    <div class="col-md-8">
+			  		<div class="card-body">
+		        		<h5 class="card-title fs-4 mt-4">Aljhun R. Angala</h5>
+		        		<p class="card-text">BSIT Student</p>
+			      	</div>
+			    </div>
+			</div>
+		</div>
+		<div class="card p-md-3 rounded-5 shadow-lg border-3 border-success" style="max-width: 1280px;">
+  			<h5 class="card-title ms-5 fs-4">Autobiography</h5>
+  			<div class="card-body">
+   	 			<h5 class="card-title">Personal Information</h5>
+    			<p class="card-text" style="margin-bottom: 2rem; text-indent: 3rem;">My name is Aljhun R. Angala, I'm 19 years old and born in Pasong Camachille, General Trias, Cavite on September 19, 2005, but I currently live in Brgy. Tampugo, Sta Cruz, Ilocos Sur with my family, we live as a middle class with ups and downs in our life.</p>
+
+    			<h5 class="card-title ">Hobbies and Favorites</h5>
+    			<p class="card-text" style="margin-bottom: 2rem; text-indent: 3rem;">My hobbies are customizing, building, and collecting gunpla model kit. Also Playing online mobile games and console games</p>
+
+    			<h5 class="card-title">Goals and Ambition</h5>
+    			<p class="card-text" style="margin-bottom: 2rem; text-indent: 3rem;">My goals are to finish my education, find a decent job and stable business and have a nice family with the person I love. But I will still gonna pursue my dream to become a game developer.</p>
+
+
+  			</div>
+		</div>
+	</div>
+</main>
+
+</x-navbtn>
