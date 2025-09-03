b8f25d4a0a2af4237fd5e0483dc0cc1e19a56230
Aljhun-7 <aljhunangala7@gmail.com>
2025-09-03 15:51:59 +0800

    Create navbtn.blade.php

---
+++ resources/views/components/navbtn.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+<p class="d-inline-flex gap-1">
+  <a href="{{ url('angala') }}" class="btn bg-custom-red custom-text sub-styled-button" >Angala, Aljhun</a>
+  <a href="{{ url('hipolito') }}" class="btn bg-custom-red custom-text sub-styled-button" >Hipolito, Charlene</a>
+  <a href="{{ url('marzan') }}" class="btn bg-custom-red custom-text sub-styled-button" >Marzan, Mark Jade</a>
+  <a href="{{ url('wong') }}" class="btn bg-custom-red custom-text sub-styled-button" >Wong, Alyza Kaye</a>
+</p>
+
+{{ $slot }}
+
+</x-layout>
